22e4a03860aab3fe86e92829ece9e11351b53a88
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-15 14:16:04 +0700

    Написать объект фильма

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,2 +1,20 @@
-package ru.yandex.practicum.filmorate.model;public class Film {
+package ru.yandex.practicum.filmorate.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private int id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private LocalDate releaseDate;
+    private int duration;
 }

51c8c8e929070d1bf231eabe55555539baf781ef
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-15 14:15:04 +0700

    Написать своё исключение для валидации

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,2 +1,7 @@
-package ru.yandex.practicum.filmorate.exceptions;public class ValidationException {
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class ValidationException extends Exception {
+    public ValidationException(final String message) {
+        super(message);
+    }
 }

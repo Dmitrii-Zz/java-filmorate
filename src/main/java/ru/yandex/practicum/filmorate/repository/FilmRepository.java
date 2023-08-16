84eae4667cc9d5dfcdf867e3fc14132e760a5ac9
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-16 22:14:15 +0700

    Написать класс для хранения данных о фильмах

--- src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
@@ -1,2 +1,33 @@
-package ru.yandex.practicum.filmorate.repository;public class FilmRepository {
+package ru.yandex.practicum.filmorate.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class FilmRepository {
+    private int filmId = 1;
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    public List<Film> findAll() {
+        return new ArrayList<>(films.values());
+    }
+
+    public boolean findFilmId(int id) {
+        return films.containsKey(id);
+    }
+    public Film save(Film film) {
+        film.setId(filmId);
+        films.put(filmId++, film);
+        return film;
+    }
+
+    public Film update(Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
 }

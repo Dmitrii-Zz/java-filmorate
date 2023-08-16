4392c62a1a14912326ba682fa22fe6fb6efd45c6
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-16 22:03:53 +0700

    Убрать код хранения данных для реализации SRP

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,106 +4,104 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.repository.UserRepository;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-
-    private boolean isValidate;
-    private int userId = 1;
-    private final Map<Integer, User> users = new HashMap<>();
+    private final UserRepository userRepository = new UserRepository();
 
     @GetMapping
     public List<User> findAll() {
-        return new ArrayList<>(users.values());
+        return userRepository.findAll();
     }
+
     @PostMapping
-    public User create(@Valid @RequestBody User user) throws ValidationException {
+    public User create(@Valid @RequestBody User user) {
 
         if (user == null) {
             log.debug("Пользователь отсутствует!");
             throw new ValidationException("Пользователь отсутствует!");
         }
 
-        isValidate = validateBirthday(user)
-            && validateLogin(user.getLogin())
-            && validateEmail(user);
-
-        if (isValidate) {
-            validateName(user);
-            user.setId(userId);
-            users.put(userId, user);
-            userId++;
-        } else {
+        if (!validate(user)) {
             log.debug("Валидация при добавления пользователя не пройдена!");
             throw new ValidationException("Проверьте правильность заполнения данных почты, логина или даты рождения.");
         }
 
-        return user;
+        validateName(user);
+        return userRepository.save(user);
     }
 
     @PutMapping
-    public User update(@Valid @RequestBody User user) throws ValidationException {
+    public User update(@Valid @RequestBody User user) {
 
-        if (!users.containsKey(user.getId())) {
+        if (!userRepository.findUserId(user.getId())) {
             log.debug("Пользователь с id '{}' отсутствует", user.getId());
             throw new ValidationException("Неверно указан id пользователя!");
         }
 
-        isValidate = validateBirthday(user)
-                && validateLogin(user.getLogin())
-                && validateEmail(user);
-
-        if (isValidate) {
-            validateName(user);
-            users.put(user.getId(), user);
-        } else {
+        if (!validate(user)) {
             log.debug("Валидация при обновлении пользователя не пройдена!");
             throw new ValidationException("Проверьте правильность заполнения данных почты, логина или даты рождения.");
         }
 
-        return user;
+        validateName(user);
+        return userRepository.update(user);
+    }
+
+    private boolean validate(User user) {
+        return validateLogin(user.getLogin())
+            && validateBirthday(user)
+            && validateEmail(user);
     }
 
     private boolean validateLogin(String login) {
 
-        if (login != null) {
-            if (login.isBlank()) return false;
-            char[] massiveCharLogin = login.toCharArray();
+        if (login == null) {
+            return false;
+        }
 
-            for (char c : massiveCharLogin) {
-                if (c == ' ') return false;
-            }
-
-        } else return false;
+        if (login.isBlank() || login.contains(" ")) {
+            return false;
+        }
 
         return true;
     }
 
     private void validateName(User user) {
         String name = user.getName();
+
         if (name == null) {
             user.setName(user.getLogin());
             return;
         }
-        if (name.isBlank()) user.setName(user.getLogin());
+
+        if (name.isBlank()) {
+            user.setName(user.getLogin());
+        }
     }
 
     private boolean validateBirthday(User user) {
-        if (user.getBirthday() == null) return false;
+
+        if (user.getBirthday() == null) {
+            return false;
+        }
+
         return LocalDate.now().isAfter(user.getBirthday());
     }
 
     private boolean validateEmail(User user) {
-        if (user.getEmail() == null) return false;
+
+        if (user.getEmail() == null) {
+            return false;
+        }
+
         return !user.getEmail().isBlank();
     }
 }

a87c430964a0e9a48ccc71c5d9c2dedce1d9bc94
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-16 22:03:37 +0700

    Убрать код хранения данных для реализации SRP

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,79 +4,67 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.repository.FilmRepository;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/films")
 @Slf4j
 public class FilmController {
-
+    private final FilmRepository filmRepository = new FilmRepository();
+    private static final int MIN_DURATION_FILM = 0;
+    private static final int MAX_LENGTH_DESCRIPTION = 200;
     private static final LocalDate VALIDATE_DATE_FILM = LocalDate.of(1895, 12, 28);
-    private boolean isValidate;
-    private int filmId = 1;
-    private final Map<Integer, Film> films = new HashMap<>();
 
     @GetMapping
     public List<Film> findAll() {
-        return new ArrayList<>(films.values());
+        return filmRepository.findAll();
     }
 
     @PostMapping
-    public Film create(@Valid @RequestBody Film film) throws ValidationException {
+    public Film create(@Valid @RequestBody Film film) {
 
         if (film == null) {
             log.debug("Фильм отсутствует!");
             throw new ValidationException("Фильм отсутствует!");
         }
 
-        isValidate = filmNameValidate(film)
-                && film.getReleaseDate().isAfter(VALIDATE_DATE_FILM)
-                && film.getDescription().length() <= 200
-                && film.getDuration() > 0;
-
-        if (isValidate) {
-            film.setId(filmId);
-            films.put(filmId, film);
-            filmId++;
-        } else {
+        if (!validate(film)) {
             log.debug("Валидация при создании фильма не пройдена!");
             throw new ValidationException("Неверно указаны имя, описание или дата релиза.");
         }
 
-        return film;
+        return filmRepository.save(film);
     }
 
     @PutMapping
-    public Film update(@Valid @RequestBody Film film) throws ValidationException {
+    public Film update(@Valid @RequestBody Film film) {
 
-        if (!films.containsKey(film.getId())) {
+        if (!filmRepository.findFilmId(film.getId())) {
             log.debug("Фильм с id '{}' отсутствует", film.getId());
             throw new ValidationException("Неверно указан id фильма!");
         }
 
-        isValidate = filmNameValidate(film)
-                && film.getReleaseDate().isAfter(VALIDATE_DATE_FILM)
-                && film.getDescription().length() <= 200
-                && film.getDuration() > 0;
-
-        if (isValidate) {
-            films.put(film.getId(), film);
-        } else {
+        if (!validate(film)) {
             log.debug("Валидация при обновления фильма не пройдена!");
             throw new ValidationException("Неверно указаны имя, описание или дата релиза.");
         }
 
-        return film;
+        return filmRepository.update(film);
     }
 
-    private boolean filmNameValidate(Film film) {
-        if (film.getName() == null) return false;
-        return !film.getName().isBlank();
+    private boolean validate(Film film) {
+
+        if (film.getName() == null) {
+            return false;
+        }
+
+        return !film.getName().isBlank()
+               && film.getReleaseDate().isAfter(VALIDATE_DATE_FILM)
+               && film.getDescription().length() <= MAX_LENGTH_DESCRIPTION
+               && film.getDuration() > MIN_DURATION_FILM;
     }
 }

26f5609a2c859d4c4da79a0d298711ec20cfaeb2
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-25 20:26:49 +0700

    Добавить функци лайков и формироварние списка популярных фильмов

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,2 +1,31 @@
-package ru.yandex.practicum.filmorate.service;public class FilmService {
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.impl.InMemoryFilmStorage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class FilmService {
+    private final InMemoryFilmStorage inMemoryFilmStorage;
+    public void addLike(int filmId, int userId) {
+        Film film = inMemoryFilmStorage.getFilmById(filmId);
+        film.getLikes().add(userId);
+    }
+
+    public void deleteLike(int filmId, int userId) {
+        Film film = inMemoryFilmStorage.getFilmById(filmId);
+        film.getLikes().remove(userId);
+    }
+
+    public List<Film> popularFilms(int count) {
+        return inMemoryFilmStorage.findAll().stream()
+                .sorted((f1, f2) -> f2.getLikes().size() - f1.getLikes().size())
+                .limit(count)
+                .collect(Collectors.toList());
+    }
 }

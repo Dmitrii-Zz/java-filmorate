df74f3b4ab8818bce543c299c2e3e14a5c51e4b4
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-25 20:52:06 +0700

    CodeStyle: убрать пробел в сигнатуре метода

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -11,14 +11,14 @@ import java.util.*;
 public class UserService {
     private final InMemoryUserStorage inMemoryUserStorage;
 
-    public void addFriend (User user, User friend) {
+    public void addFriend(User user, User friend) {
         user.getFriends().add(friend.getId());
         if (!friend.getFriends().contains(user.getId())) {
             addFriend(friend, user);
         }
     }
 
-    public void removeFriend (User user, User friend) {
+    public void removeFriend(User user, User friend) {
         user.getFriends().remove(friend.getId());
         if (friend.getFriends().contains(user.getId())) {
             removeFriend(friend, user);
@@ -40,7 +40,7 @@ public class UserService {
         return mutualFriends;
     }
 
-    public List<User> getFriends (int id) {
+    public List<User> getFriends(int id) {
 
         User user = inMemoryUserStorage.getUserById(id);
         List<User> friends = new ArrayList<>();

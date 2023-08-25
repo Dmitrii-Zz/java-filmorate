c38feb589bb71b32e07baa704965e467ec9c5492
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-25 20:31:00 +0700

    Реализовать интерфейс UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/InMemoryUserStorage.java
@@ -1,34 +1,43 @@
 package ru.yandex.practicum.filmorate.storage.impl;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.interfaces.UserStorage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-@Repository
-public class UserStorage {
+@Component
+public class InMemoryUserStorage implements UserStorage {
     private int userId = 1;
     private final Map<Integer, User> users = new HashMap<>();
 
+    @Override
     public List<User> findAll() {
         return new ArrayList<>(users.values());
     }
 
+    @Override
     public boolean findUserId(int id) {
         return users.containsKey(id);
     }
 
+    @Override
     public User save(User user) {
         user.setId(userId);
+        user.setFriends(new HashSet<>());
         users.put(userId++, user);
         return user;
     }
 
+    @Override
     public User update(User user) {
+        user.setFriends(users.get(user.getId()).getFriends());
         users.put(user.getId(), user);
         return user;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return users.get(id);
     }
 }

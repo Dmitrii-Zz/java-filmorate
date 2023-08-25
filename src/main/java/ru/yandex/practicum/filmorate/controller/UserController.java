2e9a511b7657d65bd6f4ca8c0c1a897deb9c40dc
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-25 20:20:01 +0700

    Добавить функции лайков и друзей

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.UserValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.repository.UserRepository;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.impl.InMemoryUserStorage;
+
+import static ru.yandex.practicum.filmorate.Constants.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -13,67 +16,122 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@Slf4j
 @RequiredArgsConstructor
 public class UserController {
-    private final UserRepository userRepository;
+    private final InMemoryUserStorage userStorage;
+    private final UserService userService;
 
     @GetMapping
     public List<User> findAll() {
-        return userRepository.findAll();
+        return userStorage.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable int id) {
+        validationId(id);
+        return userStorage.getUserById(id);
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
 
         if (user == null) {
-            log.debug("Пользователь отсутствует!");
-            throw new ValidationException("Пользователь отсутствует!");
+            throw new UserNotFoundException("В запросе отсутствует пользователь.");
         }
 
-        if (!validate(user)) {
-            log.debug("Валидация при добавления пользователя не пройдена!");
-            throw new ValidationException("Проверьте правильность заполнения данных почты, логина или даты рождения.");
-        }
-
+        userValidate(user);
         validateName(user);
-        return userRepository.save(user);
+        return userStorage.save(user);
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
 
-        if (!userRepository.findUserId(user.getId())) {
-            log.debug("Пользователь с id '{}' отсутствует", user.getId());
-            throw new ValidationException("Неверно указан id пользователя!");
+        if (user == null) {
+            throw new UserNotFoundException("В запросе отсутствует пользователь.");
         }
 
-        if (!validate(user)) {
-            log.debug("Валидация при обновлении пользователя не пройдена!");
-            throw new ValidationException("Проверьте правильность заполнения данных почты, логина или даты рождения.");
-        }
-
+        validationId(user.getId());
+        userValidate(user);
         validateName(user);
-        return userRepository.update(user);
+        return userStorage.update(user);
     }
 
-    private boolean validate(User user) {
-        return validateLogin(user.getLogin())
-            && validateBirthday(user)
-            && validateEmail(user);
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable int id, @PathVariable int friendId) {
+        validationIdUserAndOtherId(id, friendId);
+        User user = userStorage.getUserById(id);
+        User friend = userStorage.getUserById(friendId);
+        userService.addFriend(user, friend);
     }
 
-    private boolean validateLogin(String login) {
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        validationIdUserAndOtherId(id, friendId);
+        User user = userStorage.getUserById(id);
+        User friend = userStorage.getUserById(friendId);
+        userService.removeFriend(user, friend);
+    }
 
-        if (login == null) {
-            return false;
+    @GetMapping("/{id}/friends")
+    public List<User> findAllFriends(@PathVariable int id) {
+        validationId(id);
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> findMutualFriends(@PathVariable int id, @PathVariable int otherId) {
+        validationIdUserAndOtherId(id, otherId);
+        return userService.findMutualFriends(id, otherId);
+    }
+
+    private void validationIdUserAndOtherId(int id, int id1) {
+        validationId(id);
+        validationId(id1);
+
+        if (id == id1) {
+            throw new UserValidationException("Нельзя добавить себя в список друзей.");
         }
 
-        if (login.isBlank() || login.contains(" ")) {
-            return false;
+        if (!userStorage.findUserId(id) || !userStorage.findUserId(id1)) {
+            throw new UserNotFoundException("Пользователь с данным id ");
+        }
+    }
+
+    public void validationId(int id) {
+        if (id < CORRECT_ID) {
+            throw new UserNotFoundException(String.format("Передан неверный ИД пользователя - id = \"%d\" ", id));
         }
 
-        return true;
+        if (!(userStorage.findUserId(id))) {
+            throw new UserNotFoundException(String.format("Пользователь с \"%d\" не найден", id));
+        }
+    }
+
+    private void userValidate(User user) {
+        if (user.getLogin() == null) {
+            throw new UserValidationException("Не передан логин - Login = null");
+        }
+
+        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new UserValidationException("Логин не должен быть пустым и содержать пробелов.");
+        }
+
+        if (user.getBirthday() == null) {
+            throw new UserValidationException("Не передана дата рождения - Birthday = null");
+        }
+
+        if (!LocalDate.now().isAfter(user.getBirthday())) {
+            throw new UserValidationException("Некорректная дата рождения.");
+        }
+
+        if (user.getEmail() == null) {
+            throw new UserValidationException("Не передан email - Email = null");
+        }
+
+        if (user.getEmail().isBlank()) {
+            throw new UserValidationException("Передан пустой логин.");
+        }
     }
 
     private void validateName(User user) {
@@ -87,23 +145,5 @@ public class UserController {
         if (name.isBlank()) {
             user.setName(user.getLogin());
         }
-    }
-
-    private boolean validateBirthday(User user) {
-
-        if (user.getBirthday() == null) {
-            return false;
-        }
-
-        return LocalDate.now().isAfter(user.getBirthday());
-    }
-
-    private boolean validateEmail(User user) {
-
-        if (user.getEmail() == null) {
-            return false;
-        }
-
-        return !user.getEmail().isBlank();
     }
 }

d0872c9b7b24f00a230036b89d8bf2e576b72ace
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-08-25 20:29:33 +0700

    Написать интерфейс юзер-хранилища

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
@@ -1,2 +1,13 @@
-package ru.yandex.practicum.filmorate.storage.interfaces;public interface UserStorage {
+package ru.yandex.practicum.filmorate.storage.interfaces;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+
+public interface UserStorage {
+    List<User> findAll();
+    boolean findUserId(int id);
+    User save(User user);
+    User update(User user);
+    User getUserById(int id);
 }

8fe52e6cfd5f7c1dd5bb6149274e2dfbc41010a8
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-09-23 23:16:02 +0700

    Написать интерфейс работы с БД лайков

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/LikeFilmsStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/LikeFilmsStorage.java
@@ -1,2 +1,12 @@
-package ru.yandex.practicum.filmorate.storage.interfaces;public interface likeFilmsStorage {
+package ru.yandex.practicum.filmorate.storage.interfaces;
+
+import java.util.Set;
+
+public interface LikeFilmsStorage {
+
+    void addLike(int filmId, int userId);
+
+    void deleteLike(int filmId, int userId);
+
+    Set<Integer> getAllLikeFilmById(int filmId);
 }

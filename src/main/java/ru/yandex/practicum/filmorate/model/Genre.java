323d99ad36e726500970ea4b11f20f185edacec5
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-09-23 23:04:27 +0700

    Создать модель жанра

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,2 +1,22 @@
-package ru.yandex.practicum.filmorate.model;public class genre {
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class Genre {
+    private int id;
+    @NotNull
+    private String name;
+
+    public Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Genre(int id) {
+        this.id = id;
+    }
 }

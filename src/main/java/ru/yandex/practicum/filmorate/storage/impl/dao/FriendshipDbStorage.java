345953d91a5e00f8cb676c8ac11331e0c2755c03
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-09-23 23:19:46 +0700

    Реализовать функционал работы с друзьями через БД h2

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/FriendshipDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/FriendshipDbStorage.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.storage.impl;
+package ru.yandex.practicum.filmorate.storage.impl.dao;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
@@ -15,13 +15,13 @@ public class FriendshipDbStorage implements FriendshipStorage {
     public void addFriend(int userId, int friendId) {
         String sqlRequest = String.format("INSERT INTO friends (user_id, friend_id, friendship_status) VALUES " +
                 "('%d', '%d', '%b')", userId, friendId, true);
-
         jdbcTemplate.execute(sqlRequest);
     }
 
     @Override
     public void removeFriend(int userId, int friendId) {
-        String sqlRequest = String.format("DELETE FROM friends WHERE user_id = '%d' AND friend_id = '%d'", userId, friendId);
+        String sqlRequest =
+                String.format("DELETE FROM friends WHERE user_id = '%d' AND friend_id = '%d'", userId, friendId);
         jdbcTemplate.execute(sqlRequest);
     }
 }

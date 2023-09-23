edfd611ab39b42c2004dcd6f0e2249bd96a11f48
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-09-23 23:09:07 +0700

    Написать класс для работы с рейтигами

--- src/main/java/ru/yandex/practicum/filmorate/service/RatingService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RatingService.java
@@ -1,2 +1,27 @@
-package ru.yandex.practicum.filmorate.service;public class RatingService {
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.RatingNotFoundException;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.storage.interfaces.RatingStorage;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RatingService {
+    private final RatingStorage ratingRepository;
+
+    public List<Mpa> findAllRating() {
+        return ratingRepository.findAllRating();
+    }
+
+    public Mpa findRatingById(int id) {
+        if (id < 1 || id > 5) {
+            throw new RatingNotFoundException(String.format("Рейтинг %d не существует.", id));
+        }
+
+        return ratingRepository.findRatingById(id);
+    }
 }

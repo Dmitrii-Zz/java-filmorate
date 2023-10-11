2f976b6a239c405bf9726567e58893f60612a111
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-11 23:22:19 +0700

    Написать исключение валидации режиссера

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/DirectorIdValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/DirectorIdValidationException.java
@@ -1,2 +1,7 @@
-package ru.yandex.practicum.filmorate.exceptions;public class DirectorIdValidationException {
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class DirectorIdValidationException extends RuntimeException {
+    public DirectorIdValidationException(final String message) {
+        super(message);
+    }
 }

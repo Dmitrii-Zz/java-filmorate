9a83e74f383aee79b2af8f179e25ad2b69c5b954
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-11 23:18:57 +0700

    Написать объект режиссера

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,2 +1,20 @@
-package ru.yandex.practicum.filmorate.model;public class Director {
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.storage.interfaces.DirectorStorage;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+public class Director {
+    private int id;
+    @NotBlank
+    private String name;
+
+    public Director(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }

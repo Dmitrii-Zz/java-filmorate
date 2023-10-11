2875d19b81196985e3d6e2179545e16c913fbb00
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-11 23:19:29 +0700

    Написать контроллер для работы с режиссерами

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,2 +1,40 @@
-package ru.yandex.practicum.filmorate.controller;public class DirectorController {
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.service.DirectorService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/directors")
+@RequiredArgsConstructor
+public class DirectorController {
+    private final DirectorService directorService;
+    @GetMapping
+    public List<Director> findAll() {
+        return directorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Director getDirectorById(@PathVariable int id) {
+        return directorService.getDirectorById(id);
+    }
+
+    @PostMapping
+    public Director createDirector(@Valid @RequestBody Director director) {
+        return directorService.createDirector(director);
+    }
+
+    @PutMapping
+    public Director updateDirector(@Valid @RequestBody Director director) {
+        return directorService.updateDirector(director);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDirector(@PathVariable int id) {
+        directorService.deleteDirector(id);
+    }
 }

ce6236312ef58ab1a0b73c2c7ebe65f9f988e0ec
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-12 23:12:20 +0700

    Добавить пустую строку между методами

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/DirectorFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/DirectorFilmStorage.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.storage.interfaces;
 
 public interface DirectorFilmStorage {
+
     void addFilmByDirector(int idFilm, int idDirector);
+
     void deleteDirectorFilm(int id);
 }

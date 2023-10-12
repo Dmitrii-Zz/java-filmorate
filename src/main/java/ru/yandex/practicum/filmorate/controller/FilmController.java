09fd56f37d3e28fc2f07cd801406e8c3b2d2bf2b
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-12 22:54:37 +0700

    Удалить комментарий

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -51,9 +51,8 @@ public class FilmController {
         return filmService.popularFilms(count);
     }
 
-    //"/director/{directorId}?sortBy=[year,likes]"
     @GetMapping("/director/{directorId}")
-    public List<Film> getFilmByDirector(@PathVariable int director_id, @RequestParam String sortBy) {
-        return filmService.getFilmsByDirector(director_id, sortBy);
+    public List<Film> getFilmByDirector(@PathVariable int directorId, @RequestParam String sortBy) {
+        return filmService.getFilmsByDirector(directorId, sortBy);
     }
 }

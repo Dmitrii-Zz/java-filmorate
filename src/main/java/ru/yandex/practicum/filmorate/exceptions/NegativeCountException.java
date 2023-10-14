0723466359efd17b3cf1b00f90df8491b10ce9e1
Jiv4ii <usaaalee@gmail.com>
2023-10-14 15:14:40 +0500

    Исправление синтаксиса

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NegativeCountException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/NegativeCountException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class NegativeCountException extends RuntimeException{
+public class NegativeCountException extends RuntimeException {
     public NegativeCountException(final String message) {
         super(message);
     }

94d0837534153969f2d99e1ada2746bc1e288b28
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-18 19:52:52 +0700

    Добавить пустую строку

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmBuildingException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmBuildingException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 public class FilmBuildingException extends RuntimeException {
-    
+
     public FilmBuildingException (final String message) {
         super("Ошибка построения объекта Film: " + message);
     }

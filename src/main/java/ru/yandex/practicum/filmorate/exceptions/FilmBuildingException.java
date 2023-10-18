c5cde0a5b98eaad44a64a42d7542f564c5549d61
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-18 19:58:00 +0700

    убрать пробел в сигнатуре метода

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmBuildingException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmBuildingException.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.exceptions;
 
 public class FilmBuildingException extends RuntimeException {
 
-    public FilmBuildingException (final String message) {
+    public FilmBuildingException(final String message) {
         super("Ошибка построения объекта Film: " + message);
     }
 }

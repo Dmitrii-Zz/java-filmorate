906a43899fa98af3091561ac27490d4fe3b5e0e2
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-21 22:37:22 +0700

    refactor: изменить способ запроса к БД

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/DirectorFilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/DirectorFilmDbStorage.java
@@ -12,21 +12,19 @@ public class DirectorFilmDbStorage implements DirectorFilmStorage {
 
     @Override
     public void addFilmByDirector(int idFilm, int idDirector) {
-        String sqlRequest =
-                String.format("MERGE INTO director_film KEY (film_id, director_id) VALUES (%d, %d);",
-                        idFilm, idDirector);
-        jdbcTemplate.execute(sqlRequest);
+        String sqlRequest = "MERGE INTO director_film KEY (film_id, director_id) VALUES (?, ?);";
+        jdbcTemplate.update(sqlRequest, idFilm, idDirector);
     }
 
     @Override
     public void deleteDirectorsByFilmId(int id) {
-        String sqlRequest =
-                String.format("DELETE FROM director_film WHERE film_id = %d;", id);
-        jdbcTemplate.execute(sqlRequest);
+        String sqlRequest = "DELETE FROM director_film WHERE film_id = ?;";
+        jdbcTemplate.update(sqlRequest, id);
     }
 
     @Override
     public void deleteDirectorFilm(int id) {
-        jdbcTemplate.execute(String.format("DELETE FROM director_film WHERE director_id = %d;", id));
+        String sqlRequest = "DELETE FROM director_film WHERE director_id = ?;";
+        jdbcTemplate.update(sqlRequest, id);
     }
 }

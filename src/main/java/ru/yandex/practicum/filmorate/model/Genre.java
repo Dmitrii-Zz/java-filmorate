d81a9ddca8e6ad4075000dbe708c09b6d00268c2
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-22 22:47:01 +0700

    Добавить конструктор с одним аргументом

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -3,13 +3,10 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 @Data
-@RequiredArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Genre {
@@ -17,4 +14,8 @@ public class Genre {
     private int id;
 
     private String name;
+
+    public Genre(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -3,17 +3,19 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.Min;
 
 @Data
 @AllArgsConstructor
-@RequiredArgsConstructor
 @Builder
 public class Mpa {
     @Min(1)
     private int id;
 
     private String name;
+
+    public Mpa(int id) {
+        this.id = id;
+    }
 }
